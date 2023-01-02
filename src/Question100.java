536d5f6597aabcf8a4cd6f60cbed0c1085927ff1
Deepak0113 <mdeecreations@gmail.com>
2023-01-02 17:30:05 +0530

    Question 100

--- src/Question100.java
+++ src/Question100.java
@@ -1,5 +1,10 @@
+//100. Java Program to convert ASCHII value to character(for A-Z only) without
+//        using special functio
+
 public class Question100 {
     public static void main(String[] args) {
-        
+        int ascii = 97;
+        if(65<=ascii && ascii>=90 || 97<=ascii && ascii>=122)
+            System.out.println((char) ascii);
     }
 }
