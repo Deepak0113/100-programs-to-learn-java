5f2bcaea3b0b542ff82dc978ced1f0ab1fd7aea1
Deepak0113 <mdeecreations@gmail.com>
2023-01-02 20:03:45 +0530

    Questions 80

--- src/Question80.java
+++ src/Question80.java
@@ -1,5 +1,26 @@
+//80.Java Program to determine whether a given matrix is a sparse matrix
+
 public class Question80 {
+    static boolean isSparseMatrix(int[][] mat){
+        int row = mat.length, col = mat[0].length;
+        int count = 0;
+
+        for(int[] arr: mat){
+            for(int item: arr){
+                if(item == 0) count++;
+            }
+        }
+
+        return count > (row*col)/2;
+    }
+
     public static void main(String[] args) {
-        
+        int mat[][] = {
+                {4, 0, 10},
+                {0, 5, 0},
+                {0, 0, 6}
+        };
+
+        System.out.print(isSparseMatrix(mat));
     }
 }
