aa6b374df416bba8b9b29f967ad28b324cea02fc
Deepak0113 <mdeecreations@gmail.com>
2023-01-02 19:40:00 +0530

    Questions 82 83

--- src/Question82.java
+++ src/Question82.java
@@ -1,5 +1,32 @@
+//82.Java Program to display the lower triangular matrix
+
 public class Question82 {
+    static void displayLowerTriangleMatrix(int[][] mat){
+        for(int i=0; i<mat.length; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print(mat[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void displayLowerTriangleMatrixAlt(int[][] mat){
+        int i=0,j;
+        while(i<mat.length){
+            j = 0;
+            while(j<=i){
+                System.out.print(mat[i][j]+" ");
+                j++;
+            }
+            i++;
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        
+        int[][] mat1 = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+
+        displayLowerTriangleMatrix(mat1);
+        displayLowerTriangleMatrixAlt(mat1);
     }
 }
--- src/Question83.java
+++ src/Question83.java
@@ -1,5 +1,40 @@
+//83.Java Program to display the upper triangular matrix
+
 public class Question83 {
+    static void displayUpperTriangleMatrix(int[][] mat){
+        for(int i=0; i<mat.length; i++){
+            for(int j=0; j<mat[0].length; j++){
+                if(j>=i){
+                    System.out.print(mat[i][j]+" ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static void displayUpperTriangleMatrixAlt(int[][] mat){
+        int i=0,j;
+        while(i<mat.length){
+            j = 0;
+            while(j<mat[0].length){
+                if(j>=i){
+                    System.out.print(mat[i][j]+" ");
+                }else{
+                    System.out.print("  ");
+                }
+                j++;
+            }
+            i++;
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        
+        int[][] mat1 = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+
+        displayUpperTriangleMatrix(mat1);
+        displayUpperTriangleMatrixAlt(mat1);
     }
 }
