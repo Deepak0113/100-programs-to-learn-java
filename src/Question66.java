2bb8c60cb23233167235ae40be9fe555169f3bf2
Deepak0113 <mdeecreations@gmail.com>
2023-01-02 20:14:26 +0530

    Questions 66 69

--- src/Question66.java
+++ src/Question66.java
@@ -1,2 +1,35 @@
+//66.Java Program to left rotate the elements of an array
+
 public class Question66 {
+    static void rotateArrayLeft(int[] arr){
+        int temp = arr[0];
+        for(int i=1; i<arr.length; i++){
+            arr[i-1] = arr[i];
+        }
+        arr[arr.length-1] = temp;
+
+        for(int n: arr){
+            System.out.print(n+" ");
+        }
+        System.out.println();
+    }
+
+    static void rotateArrayLeftAlt(int[] arr){
+        int temp = arr[0], i=1;
+        while(i<arr.length){
+            arr[i-1] = arr[i];
+            i++;
+        }
+        arr[arr.length-1] = temp;
+
+        for(int n: arr){
+            System.out.print(n+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        rotateArrayLeft(new int[]{1,2,3,4,5});
+        rotateArrayLeftAlt(new int[]{1,2,3,4,5});
+    }
 }
--- src/Question69.java
+++ src/Question69.java
@@ -1,2 +1,36 @@
+//69.Java Program to rotate the elements of an array in the right direction
+
 public class Question69 {
+    static void rotateArrayRight(int[] arr){
+        int temp = arr[arr.length-1];
+        for(int i=arr.length-2; i>=0; i--){
+            arr[i+1] = arr[i];
+        }
+        arr[0] = temp;
+
+        for(int n: arr){
+            System.out.print(n+" ");
+        }
+        System.out.println();
+    }
+
+    static void rotateArrayRightAlt(int[] arr){
+        int temp = arr[arr.length-1];
+        int i=arr.length-2;
+        while(i>=0){
+            arr[i+1] = arr[i];
+            i--;
+        }
+        arr[0] = temp;
+
+        for(int n: arr){
+            System.out.print(n+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        rotateArrayRight(new int[]{1,2,3,4,5});
+        rotateArrayRightAlt(new int[]{1,2,3,4,5});
+    }
 }
