1a70ea0904738f4ad169bfac14329e8265f21501
Deepak0113 <mdeecreations@gmail.com>
2023-01-02 17:31:47 +0530

    Question 98,99

--- src/Question98.java
+++ src/Question98.java
@@ -1,5 +1,12 @@
+//98. Java Program to print the print the equivalent Capital letter of a given small letter
+//        in Alphabets.
+
 public class Question98 {
     public static void main(String[] args) {
-        
+        char ch = 'a';
+        if(Character.isAlphabetic(ch)){
+            System.out.println((char) ((int) ch - 97 + 65));
+            System.out.println(Character.toUpperCase(ch));
+        }
     }
 }
--- src/Question99.java
+++ src/Question99.java
@@ -1,5 +1,50 @@
+//99. Java Program to convert all small letter to capital letter without using special
+//        functions.
+
 public class Question99 {
+    static String convertToUpper(String string){
+        String res = "";
+        int ascii;
+        char ch;
+
+        for(int i=0; i<string.length(); i++){
+            ch = string.charAt(i);
+
+            if(Character.isAlphabetic(ch) && Character.isLowerCase(ch)){
+                ascii = (int) ch - 97 + 65;
+                res += (char) ascii;
+            } else{
+                res += ch;
+            }
+        }
+
+        return res;
+    }
+
+    static String convertToUpperAlt(String string){
+        String res = "";
+        int ascii, i=0;
+        char ch;
+
+        while(i<string.length()){
+            ch = string.charAt(i);
+
+            if(Character.isAlphabetic(ch) && Character.isLowerCase(ch)){
+                ascii = (int) ch - 97 + 65;
+                res += (char) ascii;
+            } else{
+                res += ch;
+            }
+
+            i++;
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
-        
+        String string = "This is my value";
+        System.out.println(convertToUpper(string));
+        System.out.println(convertToUpperAlt(string));
     }
 }
