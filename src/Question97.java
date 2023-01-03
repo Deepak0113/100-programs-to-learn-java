7578894151d55beddfb0242ab50404afae180d0d
Deepak0113 <mdeecreations@gmail.com>
2023-01-03 07:12:48 +0530

    Question 97

--- src/Question97.java
+++ src/Question97.java
@@ -1,5 +1,45 @@
+import java.util.Arrays;
+
 public class Question97 {
+    static boolean binarySearch(String string, char ch){
+        char[] charArr = string.toCharArray();
+        Arrays.sort(charArr);
+
+        int start=0 , end=charArr.length-1;
+        int mid;
+        while(start <= end){
+            mid = (start+end)/2;
+            if(charArr[mid] == ch) return true;
+
+            if(charArr[mid] < ch){
+                start = mid+1;
+            } else{
+                end = mid;
+            }
+        }
+
+        return false;
+    }
+
+    static boolean binarySearchRecursive(char[] charArr, char ch, int start, int end){
+        if(start > end){
+            return false;
+        }
+
+        int mid = (start+end)/2;
+        if(charArr[mid] == ch) return true;
+        if(charArr[mid] < ch)
+            return binarySearchRecursive(charArr, ch, mid+1, end);
+        return binarySearchRecursive(charArr, ch, start, mid);
+    }
+
+    static boolean binarySearchAlt(String string, char ch){
+        return binarySearchRecursive(string.toCharArray(), ch, 0, string.length()-1);
+    }
+
     public static void main(String[] args) {
-        
+        String string = "working";
+        System.out.println(binarySearch(string,'k'));
+        System.out.println(binarySearchAlt(string,'k'));
     }
 }
